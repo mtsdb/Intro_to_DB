29ee0afcdca7809732318cf25838b10ecc279e9b
mtsdb <tomatias.te@gmail.com>
2025-10-17 13:02:20 +0300

    working on Intro_to_DB create MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector 
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='your_username',
+            password='your_password'
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
